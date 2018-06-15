37d19f68bada65be50798b312d7b8d9e890a9b4c
Leonardo (NPDS) <leonardo.dutra@crateus.ufc.br>
2018-06-15 16:17:09 -0300

    #1 - finalizando exclusão e listagem clientes

--- src/br/ufc/crateus/os/beans/ClienteBean.java
+++ src/br/ufc/crateus/os/beans/ClienteBean.java
@@ -24,12 +24,8 @@ public class ClienteBean implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 
-//	private ClienteService cliServ;
-	int c = 0;
 	int count = 0;
 
-//	private List<Cliente> listClientes;
-	
 	private List<SelectItem> listClientesSelectOneMenu;
 	
 	private Cliente clienteSelecionado;
@@ -38,12 +34,13 @@ public class ClienteBean implements Serializable {
 	MessagesUtils msgUtils;
 	
 	public void novoCliente() {
-//		cliServ.salvar();
-//		cliente = new Cliente();
-		
 		
 		if(clientes == null) {
 			clientes = new ArrayList<Cliente>();
+		}
+		
+		if(clienteSelecionado == null) {
+			clienteSelecionado = new Cliente();
 		}
 		
 		if(clienteSelecionado != null) {
@@ -51,70 +48,24 @@ public class ClienteBean implements Serializable {
 			clientes.add(clienteSelecionado);
 		}
 		
-			clienteSelecionado = new Cliente();
-		
-		
-		
-		
-		
-//		listClientes.add(cliente);
-		
-//		System.out.println("Antes de novo Cliente\n " + cliente.toString());
-		
-//		System.out.println("\nDepois de novo Cliente\n");
-//		System.out.println(cliente.toString());
-		
-		
+			
 		
 		msgUtils = new MessagesUtils("Registro Salvo", "Novo Cliente Registrado!", MessagesTypes.SUCCESS);
 	}
 	
 	public ClienteBean() {
-//		cliServ = new ClienteService();
-//		cliente = new Cliente();
-//		listClientes = new ArrayList<Cliente>();
-		clienteSelecionado = new Cliente();
 
 	}
 
 	@PostConstruct
 	public void inicializar() {
+		clienteSelecionado = new Cliente();
+	}
 
-//		if(cliente != null) {
-//		System.out.println("###############################");
-//		System.out.println("Inicializando valores...");
-//		clienteSelecionado = new Cliente();
-		
-		
-//		System.out.println("nome cliente => " + cliente.getNome());
-//		System.out.println("Total de objetos cliente: " + cliente.getId());
-		System.out.println("chamada =>> " + ++c);
-		System.out.println("###############################");
-		}
-		
-//		if(cliente == null) {
-//			cliente = new Cliente();
-//		}
-		
-//		cliente = new Cliente();
-//	}
-	
-	
-	
-//	public ClienteService getCliServ() {
-//		return cliServ;
-//	}
-//
-//	public void setCliServ(ClienteService cliServ) {
-//		this.cliServ = cliServ;
-//	}
 
 	
 	public Cliente getClienteSelecionado() {
-		if(clienteSelecionado != null) {
-			return clienteSelecionado;
-		}
-		return null;
+		return clienteSelecionado;
 	}
 
 	public void setClienteSelecionado(Cliente clienteSelecionado) {
@@ -124,59 +75,28 @@ public class ClienteBean implements Serializable {
 	public Cliente searchById(int idCliente) {
 		if(clientes != null) {
 			for(Cliente c : clientes) {
-				if(c != null) {
+				
 					if(c.getId() == idCliente) {
 						return c;
 					}
-				}
+				
 			}
 		}
-		System.out.println("Pesquisa por ID em Cliente inexistente...");
+
 		return null;
-	}
-
-	public String goListCli(){
-		return "/cliente/listCliente.xhtml";
-	}
-
-	//	public Cliente getClienteSelecionado() {
-	//		return clienteSelecionado;
-	//	}
-	//
-	//	public void setClienteSelecionado(Cliente clienteSelecionado) {
-	//		this.clienteSelecionado = clienteSelecionado;
-	//	}
-
-	public List<SelectItem> getListClientesSelectOneMenu() {
-		
-		if(listClientesSelectOneMenu == null) {
-			listClientesSelectOneMenu = new ArrayList<SelectItem>();
-						
-			if(clientes != null && !clientes.isEmpty()) {
-				SelectItem item;
-				for(Cliente cliLista : clientes) {
-					item = new SelectItem(cliLista, cliLista.getNome());
-					listClientesSelectOneMenu.add(item);
-				}
-			}
-		}
-		
-		
-		return listClientesSelectOneMenu;
 	}
 
 	public List<Cliente> getClientes() {
 		return clientes;
 	}
 
-//	public Cliente getCliente() {
-//		return cliente;
-//	}
-//
-//	public void setCliente(Cliente cliente) {
-//		this.cliente = cliente;
-//	}
-	
-	
-	
+	public void excluirCliente() {
+		
+		if(getClienteSelecionado().getId() != null && clientes != null) {
+			clientes.remove(searchById(getClienteSelecionado().getId()));
+			System.out.println("excluido");
+		}else {
+			msgUtils = new MessagesUtils("Não foi possível remover! Provavelmente objetos vazios...", "Cliente vazio", MessagesTypes.WARNING);	
+		}
+	}
 }
